70aa36fe80df2ce67f80f8e8fa92efe80053eec6
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2024-11-27 12:26:02 +0545

    Course Goal Component Added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CourseGoal from "./components/CourseGoal";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal
+        title="Learn React with Typescript"
+        description="Learn it from the ground up"
+      />
+    </main>
+  );
+}
