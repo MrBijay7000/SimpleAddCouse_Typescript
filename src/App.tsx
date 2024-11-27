8d52fda14c79d440471f869c10cfc4e138a2a531
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2024-11-27 13:05:07 +0545

    Header Component Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
 
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="Learn React with Typescript"
-        description="Learn it from the ground up"
-      />
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <CourseGoal title="Learn React with Typescript">
+        <p>Learn it from the ground up</p>
+      </CourseGoal>
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,17 +1,40 @@
-export default function CourseGoal({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) {
+// type CourseGoalProps = {
+//   title: string;
+//   description: string;
+// };
+
+import { PropsWithChildren } from "react";
+
+// // //
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        <p>{children}</p>
       </div>
       <button>DELETE</button>
     </article>
   );
 }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>{children}</p>
+//       </div>
+//       <button>DELETE</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
